12abf124ea19bcea6272b0bed931d9ebf7ba7026
Gray Lewis <grayjordann@gmail.com>
2021-03-21 14:33:07 +0100

    bugfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'clock', component: ClockComponent }
+  { path: '**', component: ClockComponent }
 ];
 
 @NgModule({
